7f2ebd71e0266211a18412ffd5f2982da565e43b
elena-lucky <elena.ivanovada@gmail.com>
2024-02-12 01:16:33 +0300

    feat: add some compleated tasks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -37,8 +37,12 @@ function getIntervalArray(start, end) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  const arrLengthMax = Math.max(arr1.length, arr2.length);
+  const arr1Full = arr1.concat(Array(arrLengthMax - arr1.length).fill(0));
+  const arr2Full = arr2.concat(Array(arrLengthMax - arr2.length).fill(0));
+  const sumOfArrays = arr1Full.map((value1, index) => value1 + arr2Full[index]);
+  return sumOfArrays;
 }
 
 /**
@@ -53,8 +57,9 @@ function sumArrays(/* arr1, arr2 */) {
  *    findElement(['Array', 'Number', 'string'], 'Date') => -1
  *    findElement([0, 1, 2, 3, 4, 5], 5) => 5
  */
-function findElement(/* arr, value */) {
-  throw new Error('Not implemented');
+function findElement(arr, value) {
+  const indexElement = arr.indexOf(value);
+  return indexElement !== -1 ? indexElement : -1;
 }
 
 /**
@@ -71,8 +76,9 @@ function findElement(/* arr, value */) {
  *    findAllOccurrences([ null, undefined, null ], null) => 2
  *    findAllOccurrences([ true, 0, 1, 'true' ], true) => 1
  */
-function findAllOccurrences(/* arr, item */) {
-  throw new Error('Not implemented');
+function findAllOccurrences(arr, item) {
+  const results = arr.filter((iItem) => iItem === item);
+  return results.length;
 }
 
 /**
